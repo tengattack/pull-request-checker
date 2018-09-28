d250f5fb49b32f40878cb5f7be16ca8ee21f6396
MyYellowBlanket <MyYellowBlanket@users.noreply.github.com>
2018-09-28 14:47:12 +0800

    set format of code block to diff

--- checker/lint.go
+++ checker/lint.go
@@ -289,7 +289,7 @@ func GoLint(filePath, repoPath string) (lints []LintMessage, err error) {
 				RuleID:   ruleID,
 				Line:     int(hunk.OrigStartLine) + delta,
 				Column:   0,
-				Message:  "\n```\n" + string(hunk.Body) + "```",
+				Message:  "\n```diff\n" + string(hunk.Body) + "```",
 				Severity: lintWarning,
 			})
 		}
