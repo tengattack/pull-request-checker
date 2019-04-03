b2a84bf333b5ececacade39dadf0e8571926c250
MyYellowBlanket <MyYellowBlanket@users.noreply.github.com>
2019-04-03 19:25:54 +0800

    use new shellwords parser

--- checker/gotest_test.go
+++ checker/gotest_test.go
@@ -7,12 +7,16 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	shellwords "github.com/tengattack/go-shellwords"
 )
 
 func TestCarry(t *testing.T) {
+	assert := assert.New(t)
 	_, filepath, _, _ := runtime.Caller(0)
+	curDir := path.Dir(filepath)
 
-	out, err := carry(context.Background(), path.Dir(filepath)+"/../testdata/go", "go test ./...")
-	assert.NoError(t, err)
-	assert.NotEmpty(t, out)
+	parser := shellwords.NewParser()
+	out, err := carry(context.Background(), parser, curDir+"/../testdata/go", "go test ./...")
+	assert.NoError(err)
+	assert.NotEmpty(out)
 }
