64d98933ba6b07a1cbba38a5daf70ce41bcce7f9
MyYellowBlanket <MyYellowBlanket@users.noreply.github.com>
2019-04-24 16:05:47 +0800

    Add test

---
+++ util/github.go
@@ -0,0 +1,73 @@
+package util
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+	"sync"
+
+	"github.com/google/go-github/github"
+)
+
+// SearchGithubPR
+func SearchGithubPR(ctx context.Context, client *github.Client, repo, sha string) (int, error) {
+	if sha == "" {
+		return 0, errors.New("SHA is empty")
+	}
+	q := fmt.Sprintf("is:pr repo:%s SHA:%s", repo, sha)
+	opts := &github.SearchOptions{Sort: "created", Order: "asc"}
+	result, _, err := client.Search.Issues(ctx, q, opts)
+	if err != nil {
+		return 0, err
+	}
+	if len(result.Issues) == 0 {
+		return 0, nil
+	}
+	return result.Issues[0].GetNumber(), nil
+}
+
+func DiffCoverage(headCoverage, baseCoverage *sync.Map) string {
+	var output strings.Builder
+	output.WriteString("```diff\n")
+	headCoverage.Range(func(key, value interface{}) bool {
+		testName, _ := key.(string)
+		currentResult, _ := value.(string)
+
+		interfaceValue, _ := baseCoverage.Load(testName)
+		baseResult, _ := interfaceValue.(string)
+
+		currentPercentage, err1 := ParseFloatPercent(currentResult, 64)
+		basePercentage, err2 := ParseFloatPercent(baseResult, 64)
+		var testMsg string
+		if err1 != nil || err2 != nil {
+			testMsg += "  "
+		} else if currentPercentage > basePercentage {
+			testMsg += "+ "
+		} else if currentPercentage < basePercentage {
+			testMsg += "- "
+		} else {
+			testMsg += "  "
+		}
+		testMsg += (testName + " test coverage: " + baseResult + " -> " + currentResult + "\n")
+		output.WriteString(testMsg)
+		return true
+	})
+	output.WriteString("\n```")
+	return output.String()
+}
+
+func GetBaseSHA(client *github.Client, owner, repo string, prNum int) (string, error) {
+	opt := &github.ListOptions{Page: 1, PerPage: 1}
+	commits, _, err := client.PullRequests.ListCommits(context.Background(), owner, repo, prNum, opt)
+	if err != nil {
+		return "", err
+	}
+	var baseSHA string
+	if len(commits) > 0 {
+		if length := len(commits[0].Parents); length > 0 {
+			baseSHA = commits[0].Parents[length-1].GetSHA()
+		}
+	}
+	return baseSHA, nil
+}
---
+++ util/github_test.go
@@ -0,0 +1,54 @@
+package util
+
+import (
+	"context"
+	"regexp"
+	"strings"
+	"sync"
+	"testing"
+
+	"github.com/google/go-github/github"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSearchGithubPR(t *testing.T) {
+	require := require.New(t)
+	assert := assert.New(t)
+
+	client := github.NewClient(nil)
+	i, err := SearchGithubPR(context.Background(), client, "tengattack/unified-ci", "7988bac704d600a86bd29149c569c788f0d7cd92")
+	require.NoError(err)
+	assert.EqualValues(23, i)
+}
+
+func TestDiffCoverage(t *testing.T) {
+	assert := assert.New(t)
+
+	var head, base sync.Map
+
+	base.Store("down", "60%")
+	head.Store("down", "50%")
+
+	base.Store("up", "50%")
+	head.Store("up", "60%")
+
+	base.Store("na", "unknown")
+	head.Store("na", "50%")
+
+	output := DiffCoverage(&head, &base)
+	assert.Regexp(regexp.MustCompile("(?s)```diff\n.*\n```"), output)
+	assert.Regexp(regexp.MustCompile(`(?m)^\+.*up.*50%.*60%`), output)
+	assert.Regexp(regexp.MustCompile(`(?m)^-.*down.*60%.*50%`), output)
+	assert.Regexp(regexp.MustCompile(`(?m)^ .*na.*unknown.*50%`), output)
+}
+
+func TestGetBaseSHA(t *testing.T) {
+	require := require.New(t)
+	assert := assert.New(t)
+
+	client := github.NewClient(nil)
+	sha, err := GetBaseSHA(client, "tengattack", "unified-ci", 30)
+	require.NoError(err)
+	assert.Equal("94a32a63aa2a618a127a00954bb9965bff8939df", strings.TrimSpace(sha))
+}
