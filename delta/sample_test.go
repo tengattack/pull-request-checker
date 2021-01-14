693d7f078e4ebcaee4c5fcc6bb5f7e33e617742c
nichanglan <rhettnina@icloud.com>
2021-01-14 17:22:02 +0800

    增量测试，不用 review，测试完毕后会关闭

---
+++ delta/sample_test.go
@@ -0,0 +1,24 @@
+package delta
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Test struct {
+	in  int
+	out string
+}
+
+var tests = []Test{
+	{-1, "negative"},
+	{5, "small"},
+}
+
+func TestSize(t *testing.T) {
+	for _, test := range tests {
+		size := size(test.in)
+		assert.Equal(t, size, test.out)
+	}
+}
